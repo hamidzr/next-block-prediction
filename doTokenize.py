1213be389e0b420253fbbd7bd3a5a8726b1b3cec
Hamid Zare <dellydela@gmail.com>
2017-11-19 15:22:41 -0600

    added parallelized tokenization of the projects. added ability to load tokens and plot

--- doTokenize.py
+++ doTokenize.py
@@ -7,19 +7,34 @@ from nltk import word_tokenize
 from nltk.util import ngrams
 from collections import Counter
 import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from joblib import Parallel, delayed
+import multiprocessing
 
 parser = argparse.ArgumentParser()
-parser.add_argument("textFile", help="text document")
-parser.add_argument("output", help="generated dict of tokens as a pickle file")
+parser.add_argument("--textFile", help="text document")
+parser.add_argument("pickleFile", help="tokens file location")
 args = parser.parse_args()
+num_cores = multiprocessing.cpu_count()
 
-tokens = []
 
-with open(args.textFile, 'r') as myfile:
-    for line in tqdm(myfile.readlines()):
-        tokens += nltk.word_tokenize(line)
+sentenceTokens = []
+# load or calculate the tokens
+if (args.textFile == None):
+    # load from pickle
+    print('loading from ', args.pickleFile)
+    with open(args.pickleFile, 'rb') as f:
+        tokens = pickle.load(f)
+else:
+    with open(args.textFile, 'r') as myfile:
+        sentenceTokens = Parallel(n_jobs=num_cores)(delayed(nltk.word_tokenize)(line) for line in tqdm(myfile.readlines()))
 
-print('finished tokenizing', len(tokens), 'tokens')
-with open(args.output, 'wb') as f:
-    pickle.dump(tokens, f)
+    # concat sentenceTokens into one list
+    tokens = []
+    for sentence in sentenceTokens:
+        tokens += sentence;
+    print('finished tokenizing', len(tokens), 'tokens')
+    with open(args.pickleFile, 'wb') as f:
+        pickle.dump(tokens, f)
 
